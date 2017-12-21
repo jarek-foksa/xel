1f5d1b42e6e07f8ef892301318723e2ec6e4f0c9
Jarek Foksa <jarek@foksa.name>
2017-12-21 07:59:36 +0100

    Move StringScanner to /utils/color.js

--- utils/color.js
+++ utils/color.js
@@ -2,7 +2,6 @@
 // @copyright
 //   © 2016-2017 Jarosław Foksa
 
-import StringScanner from "../classes/string-scanner.js";
 import {normalize, round} from "./math.js";
 
 let {min, max, floor} = Math;
@@ -162,6 +161,93 @@ let namedColors = {
   yellow:               [255, 255,   0],
   yellowgreen:          [154, 205,  50]
 };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class StringScanner {
+  // @type
+  //   (string) => void
+  constructor(text) {
+    this.text = text;
+
+    this.cursor = 0;
+    this.line = 1;
+    this.column = 1;
+
+    this._storedPosition = {cursor: 0, line: 1, column: 1};
+  }
+
+  // @info
+  //   Read given number of chars.
+  // @type
+  //   (number) => string?
+  read(i = 1) {
+    let string = "";
+    let initialCursor = this.cursor;
+
+    for (let j = 0; j < i; j += 1) {
+      let c = this.text[initialCursor + j];
+
+      if (c === undefined) {
+        break;
+      }
+      else {
+        string += c;
+        this.cursor += 1;
+
+        if (c === "\n"){
+          this.line += 1;
+          this.column = 1;
+        }
+        else {
+          this.column += 1;
+        }
+      }
+    }
+
+    return (string === "" ? null : string);
+  }
+
+  // @info
+  //   Read given number of chars without advancing the cursor.
+  // @type
+  //   (number) => string?
+  peek(i = 1) {
+    let string = "";
+
+    for (let j = 0; j < i; j += 1) {
+      let c = this.text[this.cursor + j];
+
+      if (c === undefined) {
+        break;
+      }
+      else {
+        string += c;
+      }
+    }
+
+    return (string === "" ? null : string);
+  }
+
+  // @type
+  //   () => void
+  storePosition() {
+    let {cursor, line, column} = this;
+    this._storedPosition = {cursor, line, column};
+  }
+
+  // @type
+  //   () => void
+  restorePosition() {
+    let {cursor, line, column} = this._storedPosition;
+
+    this.cursor = cursor;
+    this.line = line;
+    this.column = column;
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // @info
 //   Convert color from RGB to HSL space. R, G and B components on input must be in 0-255 range.
