e78cabfb932fcbd23d0889eb7d5639ab84e009a8
Jarek Foksa <jarek@foksa.name>
2018-11-19 00:59:16 +0100

    Bugfix: don't hide the scrollbar when showing <x-backdrop>

--- elements/x-backdrop.js
+++ elements/x-backdrop.js
@@ -76,8 +76,13 @@ export class XBackdropElement extends HTMLElement {
 
     // Prevent the document body from being scrolled
     {
+      if (this._wheelListener) {
+        document.body.removeEventListener("wheel", this._wheelListener);
+        this._wheelListener = null;
+      }
+
       if (document.body.scrollHeight > document.body.clientHeight) {
-        document.body.style.overflow = "hidden";
+        document.body.addEventListener("wheel", this._wheelListener = (event) => event.preventDefault());
       }
     }
 
@@ -119,14 +124,22 @@ export class XBackdropElement extends HTMLElement {
       );
 
       backdropAnimation.finished.then(() => {
-        document.body.style.overflow = null;
+        if (this._wheelListener) {
+          document.body.removeEventListener("wheel", this._wheelListener);
+          this._wheelListener = null;
+        }
+
         this.remove();
       });
 
       return backdropAnimation.finished;
     }
     else {
-      document.body.style.overflow = null;
+      if (this._wheelListener) {
+        document.body.removeEventListener("wheel", this._wheelListener);
+        this._wheelListener = null;
+      }
+
       this.remove();
     }
   }
